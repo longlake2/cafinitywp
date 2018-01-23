34103eae707b447a1f6fd6006bd1d8258df26653
longlake2 <sites@emailhart.net>
2018-01-23 17:46:25 -0500

    inital dl plus added bsmd and js cleaned sass files

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CafinityWP Theme Customizer
+ *
+ * @package CafinityWP
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function cafinitywp_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'cafinitywp_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'cafinitywp_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'cafinitywp_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function cafinitywp_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function cafinitywp_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function cafinitywp_customize_preview_js() {
+	wp_enqueue_script( 'cafinitywp-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'cafinitywp_customize_preview_js' );
---
+++ js/manifest.js
@@ -0,0 +1,4 @@
+//= include ../node-modles/bootstrap/dist/js/bootstrap.js
+//= include ../node_modules/popper.js/dist/umd/popper.js
+//= include ../node_modules/bootstrap-material-design/dist/js/bootstrap-material-design.js
+//= include ../js/src/site.js
